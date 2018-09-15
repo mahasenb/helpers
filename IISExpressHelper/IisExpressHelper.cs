2dfbf38234e2eda8f6ffb15c1d78cd1ca9bc2300
Mahasen Bandara <pmbandara@mitrai.com>
2018-09-15 14:25:17 +0530

    Added the iis executor and configurations

---
+++ IISExpressHelper/IisExpressHelper.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Management;
+using System.Net.Http;
+using System.Threading;
+
+namespace TargetApplication.Tests.Helpers
+{
+    /// <summary>
+    /// Helps iisexpress.exe automation.
+    /// </summary>
+    public class IisExpressHelper
+    {
+        private readonly string executablePath;
+        private readonly string appPath;
+        private readonly int port;
+
+        /// <summary>
+        /// Initializes a new instance of the IisExpressHelper class with the specified parameters.
+        /// </summary>
+        /// <param name="path">The path of a iisexpress.exe file.</param>
+        /// <param name="appPath">The full physical path of the application to run.</param>
+        /// <param name="port">The port to which the application will bind.</param>
+        public IisExpressHelper(string path, string appPath, int port)
+        {
+            if (port < 1) throw new ArgumentOutOfRangeException(nameof(port));
+
+            executablePath = path ?? throw new ArgumentNullException(nameof(path));
+            this.appPath = appPath ?? throw new ArgumentNullException(nameof(appPath));
+            this.port = port;
+        }
+
+        /// <summary>
+        /// Start the web application.
+        /// </summary>
+        public void Start()
+        {
+            var arguments = string.Join(" ", CreateArguments());
+
+            using (Process.Start(executablePath, arguments))
+            {
+                var uri = new Uri($"http://localhost:{port}/");
+                var timeout = DateTime.Now + TimeSpan.FromMinutes(3);
+
+                while (DateTime.Now < timeout)
+                {
+                    using (var client = new HttpClient())
+                    {
+                        Thread.Sleep(1000);
+
+                        var response = client.GetAsync(uri).ConfigureAwait(false).GetAwaiter().GetResult();
+
+                        if ((((int)response.StatusCode) / 100) != 5) return;
+                    }
+                }
+
+                throw new TimeoutException();
+            }
+        }
+
+        public string BaseUrl => $"http://localhost:{port}/";
+
+        /// <summary>
+        /// Stop the web application.
+        /// </summary>
+        public void StopAll()
+        {
+            using (var searcher = new ManagementObjectSearcher("SELECT * FROM Win32_Process WHERE Name='iisexpress.exe'"))
+            using (var processes = searcher.Get())
+            {
+                foreach (var process in processes.OfType<ManagementObject>())
+                {
+                    using (process)
+                    {
+                        var commandLine = Convert.ToString(process["CommandLine"]);
+
+                        if (commandLine.Contains(appPath) && commandLine.Contains(port.ToString()))
+                        {
+                            try
+                            {
+                                process.InvokeMethod("Terminate", new object[] { });
+                            }
+                            catch
+                            {
+                                // ignore.
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        private IEnumerable<string> CreateArguments()
+        {
+            yield return $"/path:\"{appPath}\"";
+            yield return $"/port:{port}";
+        }
+    }
+}
---
+++ IISExpressHelper/TestRunConfigurations.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TargetApplication.Tests.Helpers
+{
+    internal class TestRunConfigurations
+    {
+        public static string IisExpressExecutablePath
+        {
+            get
+            {
+                var programFiles = Environment.GetFolderPath(Environment.SpecialFolder.ProgramFiles);
+                return programFiles + "/IIS Express/iisexpress.exe";
+            }
+        }
+
+        public static int HttpPort => 2020;
+
+        public static string TargetApplicationPath
+        {
+            get
+            {
+                var solutionFolder = Path.GetDirectoryName(Path.GetDirectoryName(Path.GetDirectoryName(AppDomain.CurrentDomain.BaseDirectory)));
+                return Path.Combine(solutionFolder ?? throw new InvalidOperationException(), "TargetApplication");
+            }
+        }
+    }
+}
